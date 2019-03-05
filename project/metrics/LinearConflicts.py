0b8519079080bd8a72b054db71b23b6cceabac75
Roman Hadiatskyi <rhadiats@student.unit.ua>
2019-03-05 22:41:57 +0200

    linear conflict v2

--- project/metrics/LinearConflicts.py
+++ project/metrics/LinearConflicts.py
@@ -4,47 +4,59 @@
 import sys
 import array
 from pprint import pprint
+
+
 # from src.BoardLogic import BoardLogic
 
+
 def GetElement(array, index):
-	return array[index] if index >= 0 and index < len(array) else None
+	return array[index] if 0 <= index < len(array) else None
 
 
-def Conflict(curr_state, final_state, index, size):
-	conflict = False
+# def conflict(curr_state, final_state, index, size):
+#     conflict = False
+#
+#     if (GetElement(curr_state, index + size) and GetElement(curr_state, index + size) == GetElement(final_state, index)
+#         and GetElement(final_state, index + size)
+#         and GetElement(final_state, index + size) == GetElement(curr_state, index)) \
+#             or (GetElement(curr_state, index - size)
+#                 and GetElement(curr_state, index - size) == GetElement(final_state, index)
+#                 and GetElement(final_state, index - size)
+#                 and GetElement(final_state, index - size) == GetElement(curr_state, index)):
+#         conflict = True
+#         print(GetElement(curr_state, index), GetElement(final_state, index))
+#
+#     return conflict
 
-	if (GetElement(curr_state, index+size) and GetElement(curr_state, index+size) == GetElement(final_state, index) \
-  	  and GetElement(final_state, index+size) and GetElement(final_state, index+size) == GetElement(curr_state, index)) \
-	  or (GetElement(curr_state, index-size) and GetElement(curr_state, index-size) == GetElement(final_state, index) \
-	  and GetElement(final_state, index-size) and GetElement(final_state, index-size) == GetElement(curr_state, index)):
-	    conflict = True
-	    print(GetElement(curr_state, index), GetElement(final_state, index))
 
-	return conflict
+def LinearMap(curr_state, final_state, size):
+	map = {}
+
+	for i in range(size**2):
+		map[i] = {curr_state[i]: {'current': {'row': [], 'col': []}, 'final': {'row': [], 'col': []}}}
+		for j in range(size):
+			map[i][curr_state[i]]['current']['row'].append(curr_state[(i // size) * size + j])
+			map[i][curr_state[i]]['current']['col'].append(curr_state[(i % size) + (size * j)])
+			map[i][curr_state[i]]['final']['row'].append(final_state[(i // size) * size + j])
+			map[i][curr_state[i]]['final']['col'].append(final_state[(i % size) + (size * j)])
+	return map
+
+
+def conflict(state, size):
+	pprint(state)
 
 
 # Get all linear conflicts in our board
 # https://algorithmsinsight.wordpress.com/graph-theory-2/a-star-in-general/implementing-a-star-to-solve-n-puzzle/
 def LinearConflicts(curr_state, final_state, size):
-  total_score = 0
+	total_score = 0
+	map = LinearMap(curr_state, final_state, size)
 
-  for i in range(size**2):
-  	if curr_state[i] != final_state[i]:
-  	  total_score += 1 if Conflict(curr_state, final_state, i, 1) else 0
-  	  total_score += 1 if Conflict(curr_state, final_state, i, size) else 0
+	for i in range(size ** 2):
+		if curr_state[i] != final_state[i]:
+			total_score += 1 if conflict(map[i], size) else 0
 
-  return total_score
-
-
-def linear_map(puzzle_len, size):
-        lin_map = []
-        for i in range(puzzle_len):
-            lin_map.append({'row': [], 'column': []})
-            for j in range(1, size):
-                lin_map[i]['column'].append((i + (size * j)) % puzzle_len)
-                lin_map[i]['row'].append(((i // size) * size) +
-                                         (((i % size) + j) % size))
-        return lin_map
+	return total_score
 
 
 if __name__ == '__main__':
@@ -59,6 +71,4 @@ if __name__ == '__main__':
 
 	curr = [3, 2, 1, 7, 0, 6, 8, 4, 5]
 	final = [1, 2, 3, 8, 0, 4, 7, 6, 5]
-	pprint(linear_map(16, 4))
-
-	# print(LinearConflicts(curr, final, 3))
+	print(LinearConflicts(curr, final, 3))
