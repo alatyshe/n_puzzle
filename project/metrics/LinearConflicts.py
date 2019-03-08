d89033643d97aaf200b2151cdcef7b20cd260ff2
Roman Hadiatskyi <rhadiats@e1r6p16.unit.ua>
2019-03-08 09:30:50 +0200

    linear v3 modified

--- project/metrics/LinearConflicts.py
+++ project/metrics/LinearConflicts.py
@@ -32,36 +32,6 @@ def LinearMap(curr_state, final_state, size):
 			map[curr_state[i]]['final']['col'].append(final_state[(i % size) + (size * j)])
 	return map
 
-
-# def conflict(map, tail):
-# 	col_diff = list(set(map[tail]['current']['col']) & set(map[tail]['final']['col'])) if tail in map[tail]['final']['col'] else []
-# 	row_diff = list(set(map[tail]['current']['row']) & set(map[tail]['final']['row'])) if tail in map[tail]['final']['row'] else []
-
-# 	column = [
-# 		(e1, e2) for (e1, e2) in
-# 		zip([e for e in map[tail]['current']['col'] if e in map[tail]['final']['col']],
-# 			[e for e in map[tail]['final']['col'] if e in map[tail]['current']['col']])
-# 		# if e1 != e2
-# 		# (e1, e2) for (e1, e2) in zip(map[tail]['current']['col'], map[tail]['final']['col'])
-# 		if e1 != e2 and (tail == e1 or tail == e2) and e1 in col_diff and e2 in col_diff
-# 	]
-# 	row = [
-# 		(e1, e2) for (e1, e2) in
-# 		zip([e for e in map[tail]['current']['row'] if e in map[tail]['final']['row']],
-# 			[e for e in map[tail]['final']['row'] if e in map[tail]['current']['row']])
-# 		# if e1 != e2
-# 		# (e1, e2) for (e1, e2) in zip(map[tail]['current']['row'], map[tail]['final']['row'])
-# 		if e1 != e2 and (tail == e1 or tail == e2) and e1 in row_diff and e2 in row_diff
-# 	]
-
-# 	# pprint(col_diff)
-# 	# pprint(row_diff)
-# 	# print(tail, ':', list(set(column) | set(row)), coef)
-# 	coef = len(col_diff) % 2 + len(row_diff) % 2
-# 	result = len(list(set(column) | set(row)))
-# 	return result // 2 + coef if result else 0
-
-
 def conflict(map, tail, size):
 	global conflicts
 
@@ -74,43 +44,32 @@ def conflict(map, tail, size):
 	final_col = map[tail]['final']['col']
 	final_row = map[tail]['final']['row']
 
-	for i, col in enumerate(col_diff):
-		try:
-			# print(col, col_diff[(i + 1) % size], 'current col:', current_col.index(col), ':', current_col.index(col_diff[(i + 1) % size]))
-			# print(col, col_diff[(i + 1) % size], 'final col:', final_col.index(col), ':', final_col.index(col_diff[(i + 1) % size]))
-			if (current_col.index(col) > current_col.index(col_diff[(i + 1) % size])\
-				and final_col.index(col) < final_col.index(col_diff[(i + 1) % size]))\
-				or (current_col.index(col) < current_col.index(col_diff[(i + 1) % size])\
-				and final_col.index(col) > final_col.index(col_diff[(i + 1) % size])):
-				conflicts += [col, col_diff[(i + 1) % size]]
-		except IndexError:
-			pass
+	# print(tail)
+	if tail not in conflicts:
+		for i, col in enumerate(col_diff):
+			try:
+				# print(col, col_diff[(i + 1) % size], 'current col:', current_col.index(col), ':', current_col.index(col_diff[(i + 1) % size]))
+				# print(col, col_diff[(i + 1) % size], 'final col:', final_col.index(col), ':', final_col.index(col_diff[(i + 1) % size]))
+				if (current_col.index(col) > current_col.index(col_diff[(i + 1) % size])\
+					and final_col.index(col) < final_col.index(col_diff[(i + 1) % size]))\
+					or (current_col.index(col) < current_col.index(col_diff[(i + 1) % size])\
+					and final_col.index(col) > final_col.index(col_diff[(i + 1) % size])):
+					conflicts += [col, col_diff[(i + 1) % size]]
+			except IndexError:
+				pass
 
-	for i, row in enumerate(row_diff):
-		try:
-			# print(row, row_diff[(i + 1) % size], 'current row:', current_row.index(row), ':', current_row.index(row_diff[(i + 1) % size]))
-			# print(row, row_diff[(i + 1) % size], 'final row:', final_row.index(row), ':', final_row.index(row_diff[(i + 1) % size]))
-			if (current_row.index(row) > current_row.index(row_diff[(i + 1) % size])\
-				and final_row.index(row) < final_row.index(row_diff[(i + 1) % size]))\
-				or (current_row.index(row) < current_row.index(row_diff[(i + 1) % size])\
-				and final_row.index(row) > final_row.index(row_diff[(i + 1) % size])):
-				conflicts += [row, row_diff[(i + 1) % size]]
-		except IndexError:
-			pass
-
-	# print(col_diff)
-	# print(row_diff)
-
-	# pprint(map[tail])
-	# print('---------------')
-
-# def conflict(curr_state, final_state, size):
-# 	for i in range(size):
-# 		for j in range(size):
-# 			try:
-# 				print(curr_state[i * size + j])
-# 			except IndexError:
-# 				pass
+	if tail not in conflicts:
+		for i, row in enumerate(row_diff):
+			try:
+				# print(row, row_diff[(i + 1) % size], 'current row:', current_row.index(row), ':', current_row.index(row_diff[(i + 1) % size]))
+				# print(row, row_diff[(i + 1) % size], 'final row:', final_row.index(row), ':', final_row.index(row_diff[(i + 1) % size]))
+				if (current_row.index(row) > current_row.index(row_diff[(i + 1) % size])\
+					and final_row.index(row) < final_row.index(row_diff[(i + 1) % size]))\
+					or (current_row.index(row) < current_row.index(row_diff[(i + 1) % size])\
+					and final_row.index(row) > final_row.index(row_diff[(i + 1) % size])):
+					conflicts += [row, row_diff[(i + 1) % size]]
+			except IndexError:
+				pass
 
 
 if __name__ == '__main__':
@@ -118,16 +77,16 @@ if __name__ == '__main__':
 	# 7 0 6		8 0 4
 	# 8 4 5		7 6 5
 
-	curr = [3, 2, 1, 7, 0, 6, 8, 4, 5]
-	final = [1, 2, 3, 8, 0, 4, 7, 6, 5]
+	# curr = [3, 2, 1, 7, 0, 6, 8, 4, 5]
+	# final = [1, 2, 3, 8, 0, 4, 7, 6, 5]
 
 	#  1  4  3 2    1  2  3  4
 	#  5  6 12 8   12 13 14  5
 	#  9 10 11 7   11  0 15  6
 	# 13 14 15 0   10  9  8  7
 
-	# curr = [1, 4, 3, 2, 5, 6, 12, 8, 9, 10, 11, 7, 13, 14, 15, 0]
-	# final = [1, 2, 3, 4, 12, 13, 14, 5, 11, 0, 15, 6, 10, 9, 8, 7]
+	curr = [1, 4, 3, 2, 5, 6, 12, 8, 9, 10, 11, 7, 13, 14, 15, 0]
+	final = [1, 2, 3, 4, 12, 13, 14, 5, 11, 0, 15, 6, 10, 9, 8, 7]
 
 	# 1 2 3		1 2 3
 	# 7 0 4		8 0 4
@@ -137,5 +96,5 @@ if __name__ == '__main__':
 	# final = [1, 2, 3, 8, 0, 4, 7, 6, 5]
 
 	# pprint(LinearMap(curr, final, 4))
-	print(LinearConflicts(curr, final, 3))
+	print(LinearConflicts(curr, final, 4))
 	# conflict(curr, final, 3)
